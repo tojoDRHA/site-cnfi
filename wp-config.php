7668a9fc51f113750465d5fc389bbf7abbae9053
Tojo <tojo.drha@gmail.com>
2022-02-02 11:48:04 +0300

    Fusion

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'sitewebcnfi2022' );
+define( 'DB_NAME', 'sitewebcnfi2021' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost:33066' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
